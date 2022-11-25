484d49a53486a2019c01b97c9963b1582dc0fc03
herley <herley@gmail.com>
2022-11-25 11:11:32 +0700

    done top query.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from decouple import config
+from pytrends.request import TrendReq
 import uvicorn
 import json
 import pandas as pd
@@ -49,6 +50,34 @@ async def getCityPerCountryTrends(country:str,city:str):
             return {'error': 'location not found!'}
     else:
         return {'error': 'country not found. please see the supported country list!'}
+
+@app.get("/youtube_get_region")
+async def youtubeGetRegion():
+    return pd.read_csv('google_region.csv')['region'].to_list()
+
+@app.get("/youtube_get_top_query")
+async def youtubeGetTopQuery(query:str,region:str,nrows:int):
+    if(query is None):
+        return {'error':'query param is null!'}
+    else:
+        locationsDF = pd.read_csv('google_region.csv')
+        locationsDF['region'] = locationsDF['region'].str.lower()
+        lokasi = locationsDF[locationsDF['region'] == region]['code'].to_list()
+        if(len(lokasi)>0 and region.isalpha()):
+            lokasi = locationsDF[locationsDF['region'] == region]['code'].to_list()[0]
+            pytrends = TrendReq(hl='en-US', tz=360)
+            kw_list = [query]
+            pytrends.build_payload(kw_list, cat=0, timeframe='today 5-y', geo=lokasi, gprop='youtube')
+            df_queries = pytrends.related_queries()
+            df_top = df_queries.get(query).get("top")
+            if(df_top is not None):
+                df_top = df_top.sort_values(['value'], ascending=False).head(nrows).reset_index(drop=True)
+                json_list = json.loads(json.dumps(list(df_top.T.to_dict().values())))
+                return json_list
+            else:
+                return {'error':'query returned no data!'}
+        else:
+            return {'error':'region does not exist!'}
 
 if __name__ == '__main__':
     uvicorn.run("app:app", host='0.0.0.0', port=5781, log_level="info", reload=True)
